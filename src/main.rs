6359b84af5a4f16ebe169b7fd4b432620a459c52
fen <fenfenus@MacBook-Air-fen.local>
2026-03-13 20:13:25 +0700

    [FEATURE] Add reading client input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use clap::Parser;
 
+#[derive(Parser)]
+struct App {
+    input: String,
+    save_to: Option<String>,
+}
+
+fn main() {
+    let app = App::parse();
+
+    let save_to = app.save_to.unwrap_or(app.input + ".svg");
 }
